c47e5c82817fd6c1c043c9d60b92a40dbc39f348
Ryan Binkley <foxtailrb@gmail.com>
2022-11-30 22:28:58 -0700

    Finished condExpressions.

--- Programming/HtDP/EdX/HtDF/HtDFDesignQuiz.rkt
+++ Programming/HtDP/EdX/HtDF/HtDFDesignQuiz.rkt
@@ -1,3 +1,23 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDFDesignQuiz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; Image Image -> Boolean
+;; Consumes two images and produces true
+;; if image1 is 'larger' than image2
+(check-expect (largerimg? (rectangle 4 5 "solid" "red")
+                          (rectangle 2 3 "solid" "red")) true)
+(check-expect (largerimg? (rectangle 4 5 "solid" "red")
+                          (rectangle 6 7 "solid" "red")) false)
+(check-expect (largerimg? (rectangle 4 5 "solid" "red")
+                          (rectangle 4 5 "solid" "red")) false)
+
+;(define (largerimg? img1 img2) false)
+
+;(define (largerimg? img1 img2)
+  ;(... img1) (... img1) > (... img2) (... img2))
+
+(define (largerimg? img1 img2)
+  ( > (+ (image-width img1) (image-height img1))
+      (+ (image-width img2) (image-height img2))))
