628e337b08cafc4397a7da0d6df2f68b373bc436
Ryan Binkley <foxtailrb@gmail.com>
2022-11-30 21:09:16 -0700

    Completed example - area.

---
+++ Programming/HtDP/EdX/Example - Area.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Example - Area|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Number -> Number
+;; Given length of one side of square, produce square's area
+(check-expect (area 3) 9)
+(check-expect (area 3.2) (* 3.2 3.2))
+
+;(define (area s) 0)
+
+;(define (area s) (... s))
+
+(define (area s) (* s s))
